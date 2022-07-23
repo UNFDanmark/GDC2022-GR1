8d66eab2e509bee2a8611ff272915691fada8b2a
UNF-GDC <109671887+UNF-GDC@users.noreply.github.com>
2022-07-23 17:20:16 +0200

    Player movement

--- GDC-project/Assets/Scripts/playerMovement.cs
+++ GDC-project/Assets/Scripts/playerMovement.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+
+    public float speed;
+    public float turnSpeed = 1;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    { 
+        float moveInput = Input.GetAxis("Vertical");//Variable for move input
+        float turnInput = Input.GetAxis("Horizontal");
+
+        float moveSpeed = speed * moveInput;
+
+        Vector3 newVelocity = transform.forward * moveSpeed;
+        newVelocity.y = rb.velocity.y;
+        gameObject.GetComponent<Transform>().Rotate(Vector3.up * turnInput * turnSpeed); // Rotates the player around Y-axis
+
+        rb.velocity = newVelocity; //Sets velocity of the player to movespeed in the forward direction
     }
 }
