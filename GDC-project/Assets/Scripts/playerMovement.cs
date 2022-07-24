1ccb330e2a8fdc77511b4db9e147b1a01409c64d
UNF-GDC <109671887+UNF-GDC@users.noreply.github.com>
2022-07-24 17:49:23 +0200

    Added player2

--- GDC-project/Assets/Scripts/playerMovement.cs
+++ GDC-project/Assets/Scripts/playerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    public bool player1 =true;
 
     public float speed;
     public float turnSpeed = 1;
@@ -30,54 +31,107 @@ public class playerMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    { 
-        float moveInput = Input.GetAxis("Vertical");//Variable for move input
-        float turnInput = Input.GetAxis("Horizontal");
-
-        float moveSpeed = speed * moveInput;
-
-        Vector3 newVelocity = transform.forward * moveSpeed;
-        newVelocity.y = rb.velocity.y;
-        rb.velocity = newVelocity; //Sets velocity of the player to movespeed in the forward direction
-
-        gameObject.GetComponent<Transform>().Rotate(Vector3.up * turnInput * turnSpeed); // Rotates the player around Y-axis
-
-        bool attackInput = Input.GetKeyDown(KeyCode.Q);
-        if (attackInput)
+    {
+        if (player1)
         {
+            float moveInput = Input.GetAxis("Vertical");//Variable for move input
+            float turnInput = Input.GetAxis("Horizontal");
 
-            attackDirection = transform.forward * attackforce;
 
-            lastAttackTime = Time.time;
+            float moveSpeed = speed * moveInput;
 
+            Vector3 newVelocity = transform.forward * moveSpeed;
+            newVelocity.y = rb.velocity.y;
+            rb.velocity = newVelocity; //Sets velocity of the player to movespeed in the forward direction
+
+            gameObject.GetComponent<Transform>().Rotate(Vector3.up * turnInput * turnSpeed); // Rotates the player around Y-axis
+
+            bool attackInput = Input.GetKeyDown(KeyCode.Q);
+            if (attackInput)
+            {
+
+                attackDirection = transform.forward * attackforce;
+
+                lastAttackTime = Time.time;
+
+            }
+
+            bool isAttacking = lastAttackTime + attackLength >= Time.time;
+
+            if (isAttacking)
+            {
+                rb.velocity = attackDirection;
+                gameObject.GetComponent<Transform>().Rotate(Vector3.up * rotationforce);
+            }
+
+            bool blockInput = Input.GetKeyDown(KeyCode.E);
+            if (blockInput)
+            {
+
+                blockDirection = transform.forward * blockForce;
+
+                lastBlockTime = Time.time;
+
+            }
+
+            bool isBlocking = lastBlockTime + blockLength >= Time.time;
+
+            if (isBlocking)
+            {
+                rb.velocity = -blockDirection;
+                gameObject.GetComponent<Transform>().Rotate(Vector3.up * -rotationforce);
+            }
         }
-
-        bool isAttacking = lastAttackTime + attackLength >= Time.time;
-
-        if (isAttacking)
+        else
         {
-            rb.velocity = attackDirection;
-            gameObject.GetComponent<Transform>().Rotate(Vector3.up * rotationforce);
+            float moveInput = Input.GetAxis("Player2 Vertical");//Variable for move input
+            float turnInput = Input.GetAxis("Player2 Horizontal");
+
+
+            float moveSpeed = speed * moveInput;
+
+            Vector3 newVelocity = transform.forward * moveSpeed;
+            newVelocity.y = rb.velocity.y;
+            rb.velocity = newVelocity; //Sets velocity of the player to movespeed in the forward direction
+
+            gameObject.GetComponent<Transform>().Rotate(Vector3.up * turnInput * turnSpeed); // Rotates the player around Y-axis
+
+            bool attackInput = Input.GetKeyDown(KeyCode.PageUp);
+            if (attackInput)
+            {
+
+                attackDirection = transform.forward * attackforce;
+
+                lastAttackTime = Time.time;
+
+            }
+
+            bool isAttacking = lastAttackTime + attackLength >= Time.time;
+
+            if (isAttacking)
+            {
+                rb.velocity = attackDirection;
+                gameObject.GetComponent<Transform>().Rotate(Vector3.up * rotationforce);
+            }
+
+            bool blockInput = Input.GetKeyDown(KeyCode.PageDown);
+            if (blockInput)
+            {
+
+                blockDirection = transform.forward * blockForce;
+
+                lastBlockTime = Time.time;
+
+            }
+
+            bool isBlocking = lastBlockTime + blockLength >= Time.time;
+
+            if (isBlocking)
+            {
+                rb.velocity = -blockDirection;
+                gameObject.GetComponent<Transform>().Rotate(Vector3.up * -rotationforce);
+            }
         }
-
-        bool blockInput = Input.GetKeyDown(KeyCode.E);
-        if (blockInput)
-        {
-
-            blockDirection = transform.forward * blockForce;
-
-            lastBlockTime = Time.time;
-
-        }
-
-        bool isBlocking = lastBlockTime + blockLength >= Time.time;
-
-        if (isBlocking)
-        {
-            rb.velocity = -blockDirection;
-            gameObject.GetComponent<Transform>().Rotate(Vector3.up * -rotationforce);
-        }
-
 
 
 
